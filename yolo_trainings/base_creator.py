b3f96b3eb577aa2cbdb4128211e9bc8e46a97f76
shorge37 <146264784+shelmingeorge@users.noreply.github.com>
2025-01-28 20:07:49 +0300

    Create base_creator.py

---
+++ yolo_trainings/base_creator.py
@@ -0,0 +1,103 @@
+import glob, os
+import cv2
+import random
+
+marker_width=5 #реальная ширина метки в см
+
+#реальная высота объектов в см 
+object_data=[
+    {"name":"pawn","number":"00"},
+    {"name":"cylinder","number":"01"},
+    {"name":"cube","number":"02"}
+]
+
+path = "path to prep images folder"
+path_to_obj = path + "\\objects"
+path_to_back = path + "\\backgrounds"
+
+path_to_base = "path to base folder"
+path_to_img = path_to_base + "train\\images\\"
+
+val_path_to_base = path_to_base + "val\\"
+val_path_to_img = val_path_to_base + "images\\"
+
+pattern = '*.jpg'
+glob_path = os.path.join(path_to_obj, pattern)
+list_files = glob.glob(glob_path)
+
+class object:
+
+    def __init__(self, obj_number):
+        self.number = obj_number
+    
+    def line(self):
+        self.w = w / new_img.shape[1]
+        self.h = h / new_img.shape[0]
+
+        self.y_c = random.randint(int(h / 2) + 1, new_img.shape[0] - int(h / 2) - 1) #  координаты центра в пикселях
+        self.x_c = random.randint(int( w / 2) + 1, new_img.shape[1] - int(w / 2) - 1)
+
+        y_c_n = self.y_c / new_img.shape[0]
+        x_c_n = self.x_c / new_img.shape[1]
+
+        line1 = str(x_c_n) + ' ' + str(y_c_n) + ' ' + str(self.w) + ' ' + str(self.h)
+        line1 = str(int(self.number)) + ' ' + line1
+        return line1
+
+    def imwrite_object_to_back(self):
+
+        new_up = self.y_c - int(h / 2)
+        new_down = self.y_c + int(h / 2)
+        new_left = self.x_c - int(w / 2)
+        new_right = self.x_c + int(w / 2)
+        
+        if img.shape[1] > new_right-new_left:
+            new_right += 1
+        if img.shape[0] > new_down-new_up:
+            new_down += 1
+
+        new_img[new_up : new_down, new_left : new_right] = img
+
+
+if list_files:
+    file_number = 0
+
+    for file_name in list_files:
+        filename = os.path.basename(file_name)
+        
+        filename_txt = path_to_base + "train\\labels\\" + filename[:-5] + ".txt"
+        val_filename_txt = val_path_to_base + "labels\\" + filename[:-5] + ".txt"
+        
+        img=cv2.imread(file_name)
+        
+        w = img.shape[1]
+        h = img.shape[0]
+
+        file_number += 1
+        i = 0
+    
+        #back_number = random.randint(1,1569) # amount of pics in folder
+        #new_img = cv2.imread(path_to_back + "\\nothing" + str(back_number) + ".jpg")
+        new_img = cv2.imread(os.path.join(path_to_back, random.choice(os.listdir(path_to_back))))
+
+        for line in object_data:   
+            if filename[:len(object_data[i]["number"])] == object_data[i]["number"]:
+                can = object(i)
+
+                if file_number% 6 == 0 :
+                    with open(val_filename_txt, 'w') as fw:
+                        fw.write(f'{can.line()}\n')
+                        fw.close()
+                        can.imwrite_object_to_back()
+                        cv2.imwrite(val_path_to_img + filename, new_img)
+                        print(filename, "validation") 
+
+                else:
+                    with open(filename_txt, 'w') as fw:
+                        fw.write(f'{can.line()}\n')
+                        fw.close()
+                        can.imwrite_object_to_back()
+                        cv2.imwrite(path_to_img + filename, new_img)
+                        print(filename) 
+            i += 1
+        
