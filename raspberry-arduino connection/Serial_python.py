33b4efc1ed14bd9d8de75bc7ac9c0a0ccd2721a1
shorge37 <146264784+shelmingeorge@users.noreply.github.com>
2025-03-14 20:22:34 +0300

    Update Serial_python.py

--- raspberry-arduino connection/Serial_python.py
+++ raspberry-arduino connection/Serial_python.py
@@ -3,18 +3,22 @@ import time
 
 num = "";
 
-def write_read(x):
-    #arduino.write(bytes(x, 'utf-8'))
+def read():
     time.sleep(0.05)
     data = arduino.readline()
     data = data.decode('utf-8')
     return data[:-2]
+
+def write(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+
 
 arduino = serial.Serial(port = 'COM3', baudrate = 115200, timeout = .1)
 
 
 while 1:
     #num = input("Enter: ")
-    value = write_read(num)
+    value = read()
     if value == "ready":
         print("yay")
